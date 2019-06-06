8f5aff87c54ddee81067151dc86cc88208c6ad84
mgarnier11 <mgarnier11@gmail.com>
2019-06-06 21:43:01 +0200

    .

--- back/src/hooks/external-api.hooks.lib.js
+++ back/src/hooks/external-api.hooks.lib.js
@@ -1,38 +1,35 @@
-const apiUtils = require('../services/apiUtils');
-const {
-  MethodNotAllowed,
-  BadRequest,
-  GeneralError
-} = require('@feathersjs/errors');
+const apiUtils = require("../services/apiUtils");
+const { BadRequest } = require("@feathersjs/errors");
 
 const apiErrors = {
-  validType: 'Please enter a valid type',
-  validRadius: 'Please enter a valid radius',
-  validLocation: 'Please enter a valid location'
+  validType: "Please enter a valid type",
+  validRadius: "Please enter a valid radius",
+  validLocation: "Please enter a valid location"
 };
 
 function beforeFindHook(options = {}) {
   return async context => {
-    const requestSrv = context.app.service('requests');
+    const requestSrv = context.app.service("requests");
     const query = context.params.query;
 
     let newQuery = {};
 
-    if (typeof query.type !== 'string')
+    if (typeof query.type !== "string")
       throw new BadRequest(apiErrors.validType);
-    newQuery.type = await context.app.service('types').get(query.type);
+    newQuery.type = await context.app.service("types").get(query.type);
 
-    if (typeof query.radius !== 'number')
+    if (typeof query.radius !== "number")
       throw new BadRequest(apiErrors.validRadius);
+
     if (query.radius <= 1750) {
       newQuery.radius = 1000;
     } else if (query.radius > 1750 && query.radius <= 3750) {
       newQuery.radius = 2500;
-    } else if (query.radius > 3750) {
+    } else {
       newQuery.radius = 5000;
     }
 
-    if (typeof query.location === 'string') {
+    if (typeof query.location === "string") {
       let town = await apiUtils.getTown(query.location);
 
       newQuery.location = {
@@ -50,15 +47,16 @@ function beforeFindHook(options = {}) {
 
       if (requests.length > 0) {
         newQuery.newRequest = false;
-        context.datas = 'test';
+        context.datas = "test";
       } else {
         newQuery.newRequest = true;
       }
-    } else if (typeof query.location === 'object') {
-      if (typeof query.location.lat !== 'number')
+    } else if (typeof query.location === "object") {
+      if (typeof query.location.lat !== "number")
         throw new BadRequest(apiErrors.validLocation);
-      if (typeof query.location.lng !== 'number')
+      if (typeof query.location.lng !== "number")
         throw new BadRequest(apiErrors.validLocation);
+
       newQuery.location = {
         lat: query.location.lat,
         lng: query.location.lng
