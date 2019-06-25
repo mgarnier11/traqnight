852e319422a2a4613ba7cb612118c7d6b833d4f4
mgarnier11 <mgarnier11@gmail.com>
2019-06-25 14:09:41 +0200

    fixed

--- front/src/js/componentsv2/App/App.jsx
+++ front/src/js/componentsv2/App/App.jsx
@@ -8,7 +8,6 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    console.log(process.env.REACT_APP_GOOGLE_MAP_API_KEY);
     this.homeRef = React.createRef();
     this.resultsRef = React.createRef();
 
